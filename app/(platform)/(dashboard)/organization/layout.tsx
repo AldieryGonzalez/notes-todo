2320922c84e96ed657f6869841641aea31515870
Aldiery Gonzalez <aldieryrgon@gmail.com>
2023-11-30 21:32:00 -0600

    Added Layout to organization

---
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -0,0 +1,19 @@
+type OrganizationLayoutProps = {
+  children: React.ReactNode;
+};
+
+const OrganizationLayout: React.FC<OrganizationLayoutProps> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <main className="mx-auto max-w-6xl px-4 pt-4 md:pt-8 2xl:max-w-screen-xl">
+      <div className="flex gap-x-7">
+        <div className="hidden w-64 shrink-0 md:block">{/* SideBar */}</div>
+        {children}
+      </div>
+    </main>
+  );
+};
+
+export default OrganizationLayout;
