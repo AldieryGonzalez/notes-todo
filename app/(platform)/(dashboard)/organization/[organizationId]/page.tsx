1ab1d847e127f0cab3d6c4fda8e89f39dcd29dbd
Aldiery Gonzalez <aldieryrgon@gmail.com>
2023-11-30 19:51:58 -0600

    Organization Id Page

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,9 @@
+type OrganizationIdPageProps = {};
+
+const OrganizationIdPage: React.FC<OrganizationIdPageProps> = ({
+  ...props
+}) => {
+  return <div>OrganizationIdPage</div>;
+};
+
+export default OrganizationIdPage;
